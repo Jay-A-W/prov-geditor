b8eee1d2c8639ae4ee2fb66b4c4faabd19ba5327
Jay Wong <jay.wong@kcl.ac.uk>
2019-01-23 15:57:01 +0000

    Creating sidebar

--- vue/index.js
+++ vue/index.js
@@ -1,3 +1,16 @@
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        activeTool: "none",
+    },
+    mutations: {
+        setActiveTool(state, activeToolName) {
+            state.activeTool = activeToolName
+        }
+    }
+})
+
 var provEditor = new Vue({
     el: '#prov-editor',
     data: {
